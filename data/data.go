a441f686c4c991feb5405a0cae5c5fe73d4d381f
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2022-02-10 13:25:46 +0530

    Completed all the required functions

--- data/data.go
+++ data/data.go
@@ -4,13 +4,25 @@ type Board [5][5]Holder
 
 // Returns a new 2D board
 func NewBoard() *Board {
-	return nil
+	board := Board{}
+	return &board
 }
 
 // FindNumber finds the given number in the board and returns
 // the row and column of the number alongwith true if value was found
 // else returns (-1, -1, false)
 func (b *Board) FindNumber(num int) (int, int, bool) {
+	i := 0
+	for i < 5 {
+		j := 0
+		for j < 5 {
+			if b[i][j].Val == num {
+				return i, j, true
+			}
+			j = j + 1
+		}
+		i = i + 1
+	}
 	return -1, -1, false
 }
 
@@ -22,14 +34,58 @@ func (b *Board) MarkValue(num int) {
 	}
 }
 
+// Checks if a row is completely marked or not
+func (b *Board) CheckBingoRow(row int) bool {
+	col := 0
+	for col < 5 {
+		if !b[row][col].Marked {
+			return false
+		}
+		col = col + 1
+	}
+	return true
+}
+
+// Checks if a column is completely marked or not
+func (b *Board) CheckBingoCol(col int) bool {
+	row := 0
+	for row < 5 {
+		if !b[row][col].Marked {
+			return false
+		}
+		row = row + 1
+	}
+	return true
+}
+
 // CheckBingo checks if the board is bingo or not
 func (b *Board) CheckBingo() bool {
+	i := 0
+	for i < 5 {
+		if b.CheckBingoRow(i) || b.CheckBingoCol(i) {
+			return true
+		}
+		i = i + 1
+	}
 	return false
 }
 
 // AddUnmarked adds the unmarked values in the board
 func (b *Board) AddUnmarked() int {
-	return 0
+	sum := 0
+	i := 0
+	for i < 5 {
+		j := 0
+		for j < 5 {
+			if !b[i][j].Marked {
+				sum = sum + b[i][j].Val
+			}
+			j = j + 1
+		}
+		i = i + 1
+	}
+
+	return sum
 }
 
 // Holder stores information about a particular
