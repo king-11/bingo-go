b363867b0d20cc877bfde3d2e97af23fccfc49fc
Abhinav Jha <linuxlenin04@gmail.com>
2022-02-14 20:32:20 +0530

    Got reader.go to work

--- play/play.go
+++ play/play.go
@@ -24,5 +24,48 @@ func FindWinner(order []int, boards []*data.Board) int {
 // returns the iteration value multiplied by addition of
 // unmarked values in the board.
 func FindLastBoard(order []int, boards []*data.Board) int {
+	// What I could do is iterate over the values and boards. Once a board goes bingo, pop it off the 'boards' slice,
+	// keep repeating it until slice's length is 1.
+
+	var bingoed = make(map[*data.Board]bool)
+	var bingoed_count int
+	var board_init_count int = len(boards)
+
+	for _, v := range boards {
+		bingoed[v] = false
+	}
+
+	for _, val := range order {
+		for _, board := range boards {
+			board.MarkValue(val)
+			bingo := board.CheckBingo()
+
+			if bingo && !bingoed[board] {
+				bingoed[board] = true
+				bingoed_count++
+			}
+
+			if bingoed_count == (board_init_count - 1) {
+				for key, _ := range bingoed {
+					if !bingoed[key] {
+						return val * key.AddUnmarked()
+					}
+				}
+			}
+
+			// For some reason the plan didn't work
+			// if bingo {
+			// 	if (board_i == 0) {
+			// 		boards = boards[1:]
+			// 	} else if (board_i < len(boards) - 1) {
+			// 		boards = append(boards[ :board_i], boards[board_i+1: ]...)
+			// 	} else {
+			// 		boards = boards[:board_i]
+			// 	}
+			// 	continue
+			// }
+			
+		}
+	}
 	return 0
 }
--- reader/io.go
+++ reader/io.go
@@ -12,7 +12,8 @@ import (
 
 // GetScanner returns a scanner for the given file.
 func GetScanner(f io.Reader) *bufio.Scanner {
-	return nil
+	new_scanner := bufio.NewScanner(f)
+	return new_scanner
 }
 
 // ReadOrder reads the order of values to mark in bingo game from the given scanner.
@@ -44,5 +45,24 @@ func ReadOrder(scanner *bufio.Scanner) ([]int, error) {
 
 // ReadBoard reads a single board from the given scanner.
 func ReadBoard(scanner *bufio.Scanner) (*data.Board, error) {
-	return nil, nil
+	var board_data [5]string
+
+	for i := 0; i < 5; i++ {
+		// Read the board as a string
+		if scanner.Scan() {
+			board_data[i] = scanner.Text()
+		}
+	}
+
+	// Convert the board_data string into a new board of all the values
+	var new_board data.Board
+		
+	for row := 0; row < 5; row++ {
+		var line_data []string = strings.Fields(board_data[row])
+		for col := 0; col < 5; col++ {
+			new_board[row][col].Val, _ = strconv.Atoi(line_data[col])
+			new_board[row][col].Marked = false
+		}
+	}
+	return &new_board, nil
 }
